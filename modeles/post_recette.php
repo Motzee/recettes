c43a48af53816f1550b5ab59f6eaa527479912f3
Maelle <maelle.perron.26@gmail.com>
2017-05-31 12:22:11 +0200

    fichier créé dans un emplacement, avec contenu issu du form (non sécurisé pour l'instant)

--- modeles/post_recette.php
+++ modeles/post_recette.php
@@ -1,13 +1,75 @@
 <?php
 
-/*on check si le formulaire est bien rempli*/
+/*on check si le formulaire est bien rempli, et on sécurise les variables*/
 $nomFichier = "test.php" ;
-$titreRecette = "Test de recette" ;
+$titreRecette = $_POST['titreRecette'] ;
+$auteurRecette = $_POST['auteurRecette'] ;
+$categRecette = $_POST['categRecette'] ;
+$photoRecette = "test" ;
+$ingredientsRecette = $_POST['ingredientsRecette'] ;
+$consignesRecette = $_POST['consignesRecette'] ;
+$astuceRecette = $_POST['astuceRecette'] ;
 
-/*Si oui, on va :*/
+//On prépare le contenu du fichier
+$contenuFichier = "<figure>
+	<img id=\"illustration-article\" src=\"img/$photoRecette\" alt=\"\" title=\"photo de la recette réalisée\" />
+</figure>
 
-$chemin = "../recettes/".$nomFichier ;
-fopen($chemin, 'w+') ; //créer un fichier
+<h2>$titreRecette</h2>
 
-echo "C'est censé être créé" ;
+<section>
+	<aside>
+    <h3>Ingrédients :</h3>
+		<ul>
+			$ingredientsRecette
+		</ul>
+        <p><em>recette proposée par : $auteurRecette</em></p>
+	</aside>
+	$consignesRecette
+    <br/>
+    Astuce : $astuceRecette
+	<hr class=\"cleareur\"/>
+</section>" ;
+
+//on détermine dans quel dossier la recette va se ranger
+$sousdossier = "" ;
+switch ($categRecette) {
+    case "Entrées" :
+        $sousdossier = "entrees" ;
+    break ;
+    case "Plats" :
+        $sousdossier = "plats" ;
+    break ;
+    case "Boissons" :
+        $sousdossier = "boissons" ;
+    break ;
+    case "Desserts" :
+        $sousdossier = "desserts" ;
+    break ;
+    case "Gâteaux" :
+        $sousdossier = "gateaux" ;
+    break ;
+    case "Glaces" :
+        $sousdossier = "glaces" ;
+    break ;
+    case "Friandises" :
+        $sousdossier = "friandises" ;
+    break ;
+    case "Non-comestibles" :
+        $sousdossier = "non-comestibles" ;
+    break ;
+    default : echo "une erreur de catégorie est survenue" ;
+}
+
+$chemin = "../recettes/".$sousdossier."/".$nomFichier ;
+
+//Création du fichier dans le bon emplacement
+$fichier = fopen($chemin, 'w+') ;
+$contenu = "je suis un test" ;
+fwrite($fichier, $contenuFichier) ;
+fclose($fichier) ;
+
+header('Location:../index.php');
+
+exit ;
 ?>
